5ab601ae87f0b53b041ff6e29bc9da1c0301e23a
sudiptabag2004 <bububag2@gmail.com>
2025-11-13 11:26:29 +0530

    usecase 5

--- Program.cs
+++ Program.cs
@@ -9,30 +9,38 @@ namespace EmployeeWage
             int IS_PART_TIME = 1;
             int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
-
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);  // 0,1,2
+            int NUM_OF_WORKING_DAYS = 20;
 
             int empHrs = 0;
             int empWage = 0;
+            int totalEmpWage = 0;
 
-            switch (empCheck)
+            Random random = new Random();
+
+            for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++)
             {
-                case 1:
-                    empHrs = 4;    // part time
-                    break;
+                int empCheck = random.Next(0, 3); // 0,1,2
 
-                case 2:
-                    empHrs = 8;    // full time
-                    break;
+                switch (empCheck)
+                {
+                    case 1:
+                        empHrs = 4;   // part-time
+                        break;
 
-                default:
-                    empHrs = 0;
-                    break;
+                    case 2:
+                        empHrs = 8;   // full-time
+                        break;
+
+                    default:
+                        empHrs = 0;   // absent
+                        break;
+                }
+
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
             }
 
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee Wage: " + empWage);
+            Console.WriteLine("Total Employee Wage for 20 Days: " + totalEmpWage);
         }
     }
 }
