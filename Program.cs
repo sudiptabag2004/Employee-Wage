fa0536bdea6735a1fadfc36e7f8bbce79f452bf3
sudiptabag2004 <bububag2@gmail.com>
2025-11-13 11:29:04 +0530

    usecase 6

--- Program.cs
+++ Program.cs
@@ -9,38 +9,44 @@ namespace EmployeeWage
             int IS_PART_TIME = 1;
             int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
+
+            int MAX_HRS_IN_MONTH = 100;
             int NUM_OF_WORKING_DAYS = 20;
 
-            int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
 
             Random random = new Random();
 
-            for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
+                int empHrs = 0;
+
                 int empCheck = random.Next(0, 3); // 0,1,2
 
                 switch (empCheck)
                 {
                     case 1:
-                        empHrs = 4;   // part-time
+                        empHrs = 4;  // part-time
                         break;
 
                     case 2:
-                        empHrs = 8;   // full-time
+                        empHrs = 8;  // full-time
                         break;
 
                     default:
-                        empHrs = 0;   // absent
+                        empHrs = 0;  // absent
                         break;
                 }
 
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
+                totalEmpHrs += empHrs;
             }
 
-            Console.WriteLine("Total Employee Wage for 20 Days: " + totalEmpWage);
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Working Days: " + totalWorkingDays);
+            Console.WriteLine("Total Hours: " + totalEmpHrs);
+            Console.WriteLine("Total Employee Wage: " + totalEmpWage);
         }
     }
 }
