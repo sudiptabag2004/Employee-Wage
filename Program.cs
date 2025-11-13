a19c94bb4649af991046e180ff309240e5ec6784
sudiptabag2004 <bububag2@gmail.com>
2025-11-13 11:21:49 +0530

    usecase 3

--- Program.cs
+++ Program.cs
@@ -6,18 +6,23 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
 
             Random random = new Random();
-            int empCheck = random.Next(0, 2); // 0 or 1
+            int empCheck = random.Next(0, 3); // 0, 1, or 2
 
             int empHrs = 0;
             int empWage = 0;
 
-            if (empCheck == IS_FULL_TIME)
+            if (empCheck == IS_PART_TIME)
             {
-                empHrs = 8;  // full day hours as per UC2 in your PDF
+                empHrs = 4;   // as per UC3
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;   // as per UC3
             }
             else
             {
@@ -25,6 +30,7 @@ namespace EmployeeWage
             }
 
             empWage = empHrs * EMP_RATE_PER_HOUR;
+
             Console.WriteLine("Employee Wage: " + empWage);
         }
     }
