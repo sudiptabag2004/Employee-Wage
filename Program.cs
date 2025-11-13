3558a6e30230d531c69adf1cf4a4fef12acf49a1
sudiptabag2004 <bububag2@gmail.com>
2025-11-13 11:24:44 +0530

    usecase 4

--- Program.cs
+++ Program.cs
@@ -11,26 +11,27 @@ namespace EmployeeWage
             int EMP_RATE_PER_HOUR = 20;
 
             Random random = new Random();
-            int empCheck = random.Next(0, 3); // 0, 1, or 2
+            int empCheck = random.Next(0, 3);  // 0,1,2
 
             int empHrs = 0;
             int empWage = 0;
 
-            if (empCheck == IS_PART_TIME)
+            switch (empCheck)
             {
-                empHrs = 4;   // as per UC3
-            }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;   // as per UC3
-            }
-            else
-            {
-                empHrs = 0;
+                case 1:
+                    empHrs = 4;    // part time
+                    break;
+
+                case 2:
+                    empHrs = 8;    // full time
+                    break;
+
+                default:
+                    empHrs = 0;
+                    break;
             }
 
             empWage = empHrs * EMP_RATE_PER_HOUR;
-
             Console.WriteLine("Employee Wage: " + empWage);
         }
     }
